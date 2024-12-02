cd42fa6577a3c00837ff85804ee5ee6ba4ae4370
Zhiend <sse_d@qq.com>
2024-12-03 01:46:11 +0800

    项目初始化

---
+++ src/main/java/com/zhiend/finetownship/FinetownshipApplication.java
@@ -0,0 +1,13 @@
+package com.zhiend.finetownship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinetownshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinetownshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhiend/finetownship/FinetownshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiend.finetownship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinetownshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
